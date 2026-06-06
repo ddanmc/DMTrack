ae5554986be033cf37105769a1fb94d5ee0666c6
Daniel Morales <dmoralesc9@gmail.com>
2026-06-06 15:29:09 -0500

    Eliminar controlador reactivo

--- src/main/java/com/paquito/primerspring/controllers/HomeController.java
+++ src/main/java/com/paquito/primerspring/controllers/HomeController.java
@@ -2,7 +2,6 @@ package com.paquito.primerspring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -10,19 +9,12 @@ public class HomeController {
     // Página de inicio (bienvenida)
     @GetMapping("/")
     public String home() {
-        return "home";
+        return "home"; // Muestra home.html
     }
 
     // Página de login personalizada
     @GetMapping("/login")
     public String login() {
-        return "login";
-    }
-
-    // Ruta de prueba para Railway
-    @GetMapping("/test")
-    @ResponseBody
-    public String test() {
-        return "DMTrack funcionando correctamente";
+        return "login"; // Muestra login.html
     }
 }
