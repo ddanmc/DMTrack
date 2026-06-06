35fe5fd9188591c1b8a853709f0952c7db2b3b86
Daniel Morales <dmoralesc9@gmail.com>
2026-06-06 15:10:07 -0500

    Agregar ruta test

--- src/main/java/com/paquito/primerspring/controllers/HomeController.java
+++ src/main/java/com/paquito/primerspring/controllers/HomeController.java
@@ -2,6 +2,7 @@ package com.paquito.primerspring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -9,12 +10,19 @@ public class HomeController {
     // Página de inicio (bienvenida)
     @GetMapping("/")
     public String home() {
-        return "home"; // Muestra home.html
+        return "home";
     }
 
     // Página de login personalizada
     @GetMapping("/login")
     public String login() {
-        return "login"; // Muestra login.html
+        return "login";
+    }
+
+    // Ruta de prueba para Railway
+    @GetMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "DMTrack funcionando correctamente";
     }
 }
